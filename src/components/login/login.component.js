23f507954e962c38a1b9ddb2005889550f457eb0
Bhanusree7 <bhanusreek12@gmail.com>
2022-08-29 11:59:06 +0530

    add: setup login style

--- src/components/login/login.component.js
+++ src/components/login/login.component.js
@@ -1,28 +1,47 @@
-import React from 'react'
-import Logo1 from "../../assets/SRMLogo.png"
-import Login from "../../assets/loginimg.png"
-import classes from "./login.module.css"
+import React from "react";
+import Logo1 from "../../assets/SRMLogo.png";
+import Login from "../../assets/loginimg.png";
+import classes from "./login.module.css";
 function LoginComponent() {
   return (
     <div className={classes.majorContainer}>
       <div className={classes.infoCtn}>
         <div>
           <div className={classes.imgwrapper}>
-            <img src={Logo1} className={classes.logo}/>
+            <img src={Logo1} className={classes.logo} />
           </div>
-          <h1>Welcome to the<br></br>iMac Portal</h1>
+          <h1>
+            Welcome to the<br></br>iMac Portal
+          </h1>
         </div>
         <div className={classes.text}>
-          <img src={Login} className={classes.image}/>
+          <img src={Login} className={classes.image} />
           <p>About | Gallery</p>
-          <p className={classes.stu}>Designed and Develpoed by students of SRM</p>
+          <p className={classes.stu}>
+            Designed and Develpoed by students of SRM
+          </p>
         </div>
       </div>
-      <div>
-
+      <div className={classes.half}>
+        <h1>Login</h1>
+        <div className={classes.sub}>
+          <p>Enter the credentials to access the portal</p>
+        </div>
+        <div className={classes.allignment}>
+          <p className={classes.entryText}>Email</p>
+          <input className={classes.entry}></input>
+          <p className={classes.entryText}>Password</p>
+          <input className={classes.entry}></input>
+          <div className={classes.color}>
+            <p>Forgot Password?</p>
+          </div>
+        </div>
+        <div className={classes.button}>
+          <button>Login</button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default LoginComponent
+export default LoginComponent;

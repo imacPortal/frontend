cc26fe9a7e7d266b1044062883499569dc5ee81a
Samaresh Samanta <ultragamer046@gmail.com>
2023-05-03 10:47:15 +0530

    test

--- src/constants/apiUrl.constant.js
+++ src/constants/apiUrl.constant.js
@@ -1,2 +1,2 @@
-export const API_URI = 'https://0imac0-backend-.up.railway.app'
-// export const API_URI = 'http://localhost:5000'
+// export const API_URI = 'https://0imac0-backend-.up.railway.app'
+export const API_URI = 'http://172.20.10.3:5000'
